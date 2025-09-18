38a1bf41e9f1241027e9e725afa7c71440ca05c1
Mostafafelfla <mostafafelfla1@gmail.com>
2025-09-18 20:32:34 +0300

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -6,22 +6,21 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
-// إعدادات الاتصال بقاعدة البيانات
-$db_host = 'localhost'; // عادة ما يكون 'localhost' في XAMPP
-$db_user = 'root';      // اسم المستخدم الافتراضي في XAMPP
-$db_pass = '';          // كلمة المرور الافتراضية في XAMPP فارغة
-$db_name = 'vet_nour_db'; // اسم قاعدة البيانات الجديد الذي حددته
+// إعدادات الاتصال بقاعدة البيانات الجديدة على Wasmer
+$db_host = 'db.fr-pari1.bengt.wasmernet.com';
+$db_port = 10272;
+$db_user = 'c41729a076d38000c7e0a7a3ffdb';
+$db_pass = '068cc417-29a0-789b-8000-d39dfa5d69a7';
+$db_name = 'dbnrpGyXBAQiLNPFWkedQS7T';
 
 // إنشاء اتصال جديد بقاعدة البيانات
-$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name, $db_port);
 
 // التحقق من وجود أخطاء في الاتصال
 if ($conn->connect_error) {
-    //
     die("Connection failed: " . $conn->connect_error);
 }
 
 // ضبط الترميز لضمان دعم اللغة العربية بشكل صحيح
 $conn->set_charset("utf8mb4");
 ?>
-
