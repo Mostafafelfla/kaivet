e0e71221c8f1b7b2f4913d693f2159fa8cc8f05c
Mostafafelfla <mostafafelfla1@gmail.com>
2025-09-19 04:30:35 +0300

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,7 @@
 <?php
 // db_connect.php
 
+// معلومات الاتصال بالقاعدة
 $host = "db.fr-pari1.bengt.wasmernet.com";
 $port = 10272;
 $dbname = "dbc3TunoJnM6JPhQfR7ShYkA";
@@ -8,12 +9,14 @@ $username = "caf2039c730c80001e8d33114ed9";
 $password = "068ccaf2-039c-74cc-8000-d5b9ce7d59dd";
 
 // إنشاء الاتصال
-$mysqli = new mysqli($host, $username, $password, $dbname, $port);
+$conn = new mysqli($host, $username, $password, $dbname, $port);
 
 // التحقق من الاتصال
-if ($mysqli->connect_error) {
-    die("فشل الاتصال بقاعدة البيانات: " . $mysqli->connect_error);
+if ($conn->connect_error) {
+    die("فشل الاتصال بقاعدة البيانات: " . $conn->connect_error);
 }
 
-// تعيين الترميز
-$mysqli->set_charset("utf8mb4");
+// ضبط الترميز
+$conn->set_charset("utf8mb4");
+
+// يمكنك الآن استخدام $conn في كل الملفات
